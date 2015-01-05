2b947d9d7b106635215cca32aa981919a15ba853
D4GGe <D4GGe92@gmail.com>
2015-01-06 00:28:22 +0100

    more go stuff

--- go/main.go
+++ go/main.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	_ "strings"
+	"strings"
 )
 import _ "github.com/go-sql-driver/mysql"
 
@@ -15,34 +15,38 @@ type Customer struct {
 }
 
 type Message struct {
-	User int
+	User string
 	Text string
 	Time string
+	Id   int
 }
 
 func getChat(w http.ResponseWriter, r *http.Request) {
+	lastId := r.FormValue("id")
+	fmt.Printf(lastId)
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	//url := strings.Split(r.URL.Path[1:], "/")
-	//args := url[len(url)-1]
+	url := strings.Split(r.URL.Path[1:], "/")
+	args := url[len(url)-1]
+	//fmt.Printf("%s", args)
 	db, err := sql.Open("mysql", "root:@/webchat")
 	if err != nil {
 		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
 	}
 	defer db.Close()
 
-	stmtOut, err := db.Prepare("SELECT user, text, time FROM messenges")
+	stmtOut, err := db.Prepare("SELECT messenges.id, user.name, messenges.text, messenges.time FROM chatrooms INNER JOIN messenges ON messenges.room = chatrooms.id INNER JOIN user ON user.id = messenges.user WHERE chatrooms.name=? and messenges.id >? ORDER BY messenges.id")
 	if err != nil {
 		panic(err.Error()) // proper error handling instead of panic in your app
 	}
 	defer stmtOut.Close()
 
-	rows, _ := stmtOut.Query()
+	rows, _ := stmtOut.Query(args, lastId)
 	msgs := []Message{}
 	for rows.Next() {
 		msg := Message{}
-		rows.Scan(&msg.User, &msg.Text, &msg.Time)
+		rows.Scan(&msg.Id, &msg.User, &msg.Text, &msg.Time)
 		msgs = append(msgs, msg)
-		fmt.Println(msg)
+
 	}
 
 	t, err := json.MarshalIndent(msgs, "", "  ")
